14c0131ac09963666d289017538066757c914922
Nikola Kolevski <nikola@solflare.com>
2025-11-09 00:58:32 +0100

    Migrate to createRoot from ReactDOM

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import ReactGA from 'react-ga';
 import './index.css';
 import App from './App';
@@ -11,20 +11,21 @@ import { useHashLocation } from "wouter/use-hash-location";
 ReactGA.initialize('UA-189112106-1');
 ReactGA.pageview(window.location.pathname + window.location.search);
 
-ReactDOM.render(
+const container = document.getElementById('root')
+const root = createRoot(container!);
+root.render(
   <React.StrictMode>
     <Router hook={useHashLocation} base="/tarkov-debrief">
       <Switch>
         <Route path="/app/:map">
-          {(params) => <App />}
+          {() => <App />}
         </Route>
         <Route path="/">
           <MapSelector />
         </Route>
       </Switch>
     </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
